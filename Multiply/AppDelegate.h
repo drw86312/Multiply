6a6a674716afaab86f158919dd4d28d33c3df748
drw86312 <david.warner4@gmail.com>
2014-05-12 15:13:08 -0500

    Initial Commit

---
+++ Multiply/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Multiply
+//
+//  Created by David Warner on 5/12/14.
+//  Copyright (c) 2014 DRWlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Multiply/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Multiply
+//
+//  Created by David Warner on 5/12/14.
+//  Copyright (c) 2014 DRWlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
